15269c831f1d912f3bb409e4071d0ed6fa8d2b64
Nick DiStefano <NDistefano@urbn.com>
2014-06-17 07:55:16 -0400

    fixed main bug

--- src/main/java/testflight/TestflightUploaderMain.java
+++ src/main/java/testflight/TestflightUploaderMain.java
@@ -16,7 +16,7 @@ public class TestflightUploaderMain {
     }
 
     private static void upload(String[] args) throws Exception {
-        TestflightUploader uploader = new TestflightUploader(null, false, null);
+        TestflightUploader uploader = new TestflightUploader(null, false, null, null);
         uploader.setLogger(new TestflightUploader.Logger() {
             public void logDebug(String message) {
                 System.out.println(message);
